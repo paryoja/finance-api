3959609f7c3a79c53807e56bb102a7bbeffddfd0
Yoonjae Park <paryoja@gmail.com>
2022-03-13 01:16:30 +0900

    add default models/admin

--- finance_api/bank/admin.py
+++ finance_api/bank/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
+from finance_api.bank import models
+
+
 # Register your models here.
+@admin.register(models.Bank)
+class BankAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+
+
+@admin.register(models.Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ["id", "bank", "name", "alias", "type"]
+
+
+@admin.register(models.Snapshot)
+class SnapshotAdmin(admin.ModelAdmin):
+    list_display = ["id", "account", "date", "amount"]
+
+
+@admin.register(models.Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ["datetime", "account_from", "account_to", "amount"]
+
+
+@admin.register(models.Withdraw)
+class WithdrawAdmin(admin.ModelAdmin):
+    list_display = ["datetime", "account", "amount"]
+
+
+@admin.register(models.Deposit)
+class DepositAdmin(admin.ModelAdmin):
+    list_display = ["datetime", "account", "amount"]
--- finance_api/bank/models.py
+++ finance_api/bank/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
+
 # Create your models here.
+class Bank(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class AccountType(models.TextChoices):
+    SAVINGS_ACCOUNT = "SAVINGS_ACCOUNT", "자유입출금"
+    INSTALLMENT_SAVING = "INSTALLMENT_SAVING", "적금"
+    TIME_DEPOSIT = "TIME_DEPOSIT", "예금"
+
+
+class Account(models.Model):
+    bank = models.ForeignKey(Bank, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    alias = models.CharField(max_length=200, blank=True, null=True)
+    type = models.CharField(
+        max_length=30, choices=AccountType.choices, default=AccountType.SAVINGS_ACCOUNT
+    )
+
+    def __str__(self):
+        return self.bank.name + ":" + self.name
+
+
+class Snapshot(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    date = models.DateField()
+
+
+class Transaction(models.Model):
+    account_from = models.ForeignKey(
+        Account, related_name="account_from", on_delete=models.CASCADE
+    )
+    account_to = models.ForeignKey(
+        Account, related_name="account_to", on_delete=models.CASCADE
+    )
+    amount = models.PositiveIntegerField()
+    datetime = models.DateTimeField()
+    note = models.TextField(null=True, blank=True)
+
+
+class Withdraw(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField()
+    datetime = models.DateTimeField()
+    note = models.TextField()
+
+
+class Deposit(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField()
+    datetime = models.DateTimeField()
+    note = models.TextField()
