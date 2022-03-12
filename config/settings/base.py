0988c8912abae44f66ad8fbebb90104af631feb7
Yoonjae Park <paryoja@gmail.com>
2022-03-13 00:13:34 +0900

    add new app bank

--- config/settings/base.py
+++ config/settings/base.py
@@ -80,7 +80,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "finance_api.users",
-    # Your stuff: custom apps go here
+    "finance_api.bank",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
